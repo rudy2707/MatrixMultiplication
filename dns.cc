5556781d16b7e254c2b4344b7427f9e4e0e7c566
Rudolf Hohn <rudolf.hohn@etu.hesge.ch>
2015-12-07 15:25:02 +0100

    DNS working

--- dns.cc
+++ dns.cc
@@ -16,21 +16,25 @@
  * =====================================================================================
  */
 
+#include <mpi.h>
 #include "dns.h"
+#include <cmath>
 #include "fox.h" // To multiply the matrix
 
 void dns(int* matLocA, int* matLocB, int* matLocC, int nloc) { 
     // Init of MPI
-    MPI_Init(&argc,&argv);
     int nbPE,myPE;
     MPI_Comm_size(MPI_COMM_WORLD,&nbPE);
     MPI_Comm_rank(MPI_COMM_WORLD,&myPE);
     
+    // Variables for the size of the matrix
+    int p = pow(nbPE + 0.1, 1.0 / 3.0);
+
     // Making the communications channels
     MPI_Comm comm_a, comm_b, comm_c;
-    MPI_Comm_split(MPI_COMM_WORLD, myPE%3+((myPE/9)*9), myPE, &comm_a);
-    MPI_Comm_split(MPI_COMM_WORLD, myPE%9, myPE, &comm_b);
-    MPI_Comm_split(MPI_COMM_WORLD, myPE/3, myPE, &comm_c);
+    MPI_Comm_split(MPI_COMM_WORLD, myPE%p+((myPE/(p*p))*(p*p)), myPE, &comm_a);
+    MPI_Comm_split(MPI_COMM_WORLD, myPE%(p*p), myPE, &comm_b);
+    MPI_Comm_split(MPI_COMM_WORLD, myPE/p, myPE, &comm_c);
     
     // Broadcasting the A and B matrix
     MPI_Bcast(matLocA, nloc*nloc, MPI_INT, 0, comm_a);
@@ -40,5 +44,10 @@ void dns(int* matLocA, int* matLocB, int* matLocC, int nloc) {
     multMatrix(matLocA, nloc, nloc, matLocB, nloc, nloc, matLocC);
 
     // Reducing the results to the C matrix
-    MPI_Reduce(matLocC, matLocC, nloc*nloc, MPI_INT, MPI_SUM, 0, comm_c);
+    //int result[nloc*nloc];
+    int* result = new int[nloc * nloc];
+    MPI_Reduce(matLocC, result, nloc*nloc, MPI_INT, MPI_SUM, 0, comm_c);
+    
+    // Copy the result in matLocC
+    for (int i = 0; i < nloc*nloc; i++) matLocC[i] = result[i];
 }
